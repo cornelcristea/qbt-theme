784733b2034b64b289fc1b88ef1c2d32b8b2eb95
cornelcristea <cornelcristea97@gmail.com>
2025-02-05 19:32:09 +0200

    update build scripts

--- Builds/build-mumble-dark.bat
+++ Builds/build-mumble-dark.bat
@@ -1,26 +1,19 @@
 @echo off
 
-pip install -r requirements.txt
-
 set ThemeDir=mumble-theme
 set IconsDir=mumble-icons
 set ThemeName=mumble-dark
+
+pip install -r requirements.txt
 
 pushd %ThemeDir%\source
     qtsass -o ..\Dark.qss Dark.scss
 popd
 
-python fill-icons.py nowshed #4B9CD3 %IconsDir%
-
-rem for /r icons/nowshed/ %%i in (*.svg) do powershell -Command "(gc %%~dpnxi) -replace '#4285f4', '#6a1b9a' | Out-File -encoding ASCII %IconsDir%/%%~nxi"
-
 python make-resource.py -base-dir %ThemeDir% ^
                         -find-files ^
                         -config dark-config.json ^
-                        -icons-dir %IconsDir% ^
                         -output %ThemeName% ^
+                        -icons-dir %IconsDir% ^
                         -style dark.qss 
 
-rd /q /s %IconsDir%
-
-rem python make-resource.py -base-dir %ThemeDir% -find-files -config dark-config.json -icons-dir icons\nowshed -output mumble-dark-nowshed -style dark.qss 
